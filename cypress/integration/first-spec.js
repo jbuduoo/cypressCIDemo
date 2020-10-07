591c5087b36db05a5f0fd034d34905f0ed6978d6
jbuduoo <43243314+jbuduoo@users.noreply.github.com>
2020-10-07 14:25:55 +0800

    20201007

--- cypress/integration/first-spec.js
+++ cypress/integration/first-spec.js
@@ -8,6 +8,11 @@ describe('第一个用例', () => {
     })
   
     it('[smoke]测试 Module API', () => {
-      expect(2).to.equal(1)
+      expect(2).to.equal(2)
     })
+
+    it('隨便增加一些東西', () => {
+      expect(2).to.equal(2)
+    })
+
   })
